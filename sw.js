123fcaba3078af19cdc6e641076f690dea6b4411
BG-user-OK <GitHub@bogdangawel.pl>
2026-05-05 07:42:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker dla Tarczy czasu pracy
 // WAŻNE: przy każdej nowej wersji aplikacji zmień nazwę cache - stare wersje automatycznie się usuną.
-// vGPT_2.8: based on a previously developed v:1.28 version created by another AI.
-const CACHE_NAME = 'tarcza-vGPT-2.8';
+// vGPT_2.9: based on a previously developed v:1.28 version created by another AI.
+const CACHE_NAME = 'tarcza-vGPT-2.9';
 
 const FILES_TO_CACHE = [
   './',
